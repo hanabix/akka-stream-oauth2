ceb8d7e98a6f2f3d7f83422f74720d4ca41f80a9
zhongl <zhong.lunfu@gmail.com>
2019-01-08 18:24:06 +0800

    Support dingtalk.

---
+++ dingtalk/src/main/scala/zhongl/stream/oauth2/dingtalk/Ding.scala
@@ -0,0 +1,129 @@
+package zhongl.stream.oauth2.dingtalk
+import java.net.URLEncoder
+import java.util.Base64
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods.POST
+import akka.http.scaladsl.model.Uri.Query
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.Location
+import akka.http.scaladsl.util.FastFuture
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.util.ByteString
+import com.auth0.jwt.algorithms.Algorithm
+import zhongl.stream.oauth2.FreshToken.InvalidToken
+import zhongl.stream.oauth2._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class Ding(authenticated: (UserInfo, Uri) => HttpResponse)(implicit system: ActorSystem) extends OAuth2[AccessToken] {
+  import Ding._
+
+  implicit val mat: Materializer    = ActorMaterializer()
+  implicit val ec: ExecutionContext = system.dispatcher
+
+  private val http                            = Http()
+  private val config                          = system.settings.config.getConfig("dingtalk.ding")
+  private val appid                           = config.getString("appid")
+  private val secret                          = config.getString("secret")
+  private val `authorization.uri`             = Uri(config.getString("authorization.uri"))
+  private val `api.uri.access-token`          = Uri(config.getString("api.uri.access-token"))
+  private val `api.uri.user-get`              = Uri(config.getString("api.uri.user-get"))
+  private val `api.uri.user-info-by-code`     = Uri(config.getString("api.uri.user-info-by-code"))
+  private val `api.uri.get-userid-by-unionid` = Uri(config.getString("api.uri.get-userid-by-unionid"))
+  private val fixedQueryString                = s"redirect_uri=${URLEncoder.encode(redirect.toString(), "utf-8")}&state="
+
+  override def refresh: Future[AccessToken] = {
+    http
+      .singleRequest(HttpRequest(uri = `api.uri.access-token`))
+      .map(complainIllegalResponse { case Content(AccessTokenE(token)) => token })
+  }
+
+  override def authenticate(token: AccessToken, request: HttpRequest): Future[HttpResponse] = {
+    val q = request.uri.query()
+    (for (code <- q.get("code"); state <- q.get("state")) yield {
+      userInfo(code, token.`access_token`).map(authenticated(_, base64Decode(state)))
+    }).getOrElse(FastFuture.successful(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity("missing code or state"))))
+  }
+
+  override def authorization(state: String): Location = {
+    val qs = s"$fixedQueryString${base64Encode(state)}"
+    Location(`authorization.uri`.withRawQueryString(`authorization.uri`.rawQueryString.map(_ + s"&$qs").getOrElse(qs)))
+  }
+
+  override def redirect: Uri = Uri(config.getString("authorization.redirect"))
+
+  private def userInfo(code: String, token: String): Future[UserInfo] = {
+    http
+      .singleRequest(getUserInfoBy(code))
+      .map(complainIllegalResponse {
+        case Content(PrincipalE(p)) => p.`user_info`.`unionid`
+      })
+      .flatMap { uid =>
+        http.singleRequest(getUseridBy(uid, token))
+      }
+      .map(complainIllegalResponse {
+        case Content(UserIdE(uid))             => uid.userid
+        case Content(ErrE(Err(40014 | 42001))) => throw InvalidToken
+      })
+      .flatMap { uid =>
+        http.singleRequest(getUserBy(uid, token))
+      }
+      .map(complainIllegalResponse {
+        case Content(UserInfoE(info))          => info
+        case Content(ErrE(Err(40014 | 42001))) => throw InvalidToken
+      })
+  }
+
+  private def getUserInfoBy(code: String) = {
+    val timestamp = System.currentTimeMillis().toString
+    val uri = `api.uri.user-info-by-code`.withQuery(
+      Query(
+        "timestamp" -> timestamp,
+        "accessKey" -> appid,
+        "signature" -> sign(timestamp, secret)
+      )
+    )
+    HttpRequest(POST, uri, entity = HttpEntity(ContentTypes.`application/json`, s"""{"tmp_auth_code":"$code"}"""))
+  }
+
+  private def getUserBy(uid: String, token: String) = {
+    HttpRequest(uri = `api.uri.user-get`.withQuery(queryWith(token, "userid" -> uid)))
+  }
+
+  private def getUseridBy(unionid: String, token: String) = {
+    HttpRequest(uri = `api.uri.get-userid-by-unionid`.withQuery(queryWith(token, "unionid" -> unionid)))
+  }
+
+  @inline
+  private def queryWith(token: String, q: (String, String)*) = Query(("access_token" -> token) +: q: _*)
+}
+
+object Ding extends JsonSupport {
+  object AccessTokenE {
+    def unapply(arg: ByteString): Option[AccessToken] = arg.as[AccessToken].toOption
+  }
+
+  object PrincipalE {
+    def unapply(arg: ByteString): Option[Principal] = arg.as[Principal].toOption
+  }
+
+  object UserIdE {
+    def unapply(arg: ByteString): Option[UserId] = arg.as[UserId].toOption
+  }
+
+  object UserInfoE {
+    def unapply(arg: ByteString): Option[UserInfo] = arg.as[UserInfo].toOption
+  }
+
+  object ErrE {
+    def unapply(arg: ByteString): Option[Err] = arg.as[Err].toOption
+  }
+
+  def sign(content: String, secret: String): String =
+    new String(Base64.getEncoder.encode(Algorithm.HMAC256(secret).sign(content.getBytes("UTF-8"))))
+
+  def apply(authenticated: (UserInfo, Uri) => HttpResponse)(implicit system: ActorSystem): Ding = new Ding(authenticated)
+
+}
---
+++ dingtalk/src/test/scala/zhongl/stream/oauth2/dingtalk/DingSpec.scala
@@ -0,0 +1,139 @@
+package zhongl.stream.oauth2.dingtalk
+import java.util.Base64
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.model.headers.Location
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives
+import akka.stream.ActorMaterializer
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+import zhongl.stream.oauth2.FreshToken.InvalidToken
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext}
+
+class DingSpec extends WordSpec with Matchers with BeforeAndAfterAll with Directives with JsonSupport {
+  import DingSpec._
+
+  implicit val tmpAuthCodeF = jsonFormat1(TmpAuthCode)
+
+  implicit val system: ActorSystem    = ActorSystem(getClass.getSimpleName)
+  implicit val mat: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContext   = system.dispatcher
+
+  private val default: (UserInfo, Uri) => HttpResponse = (_, _) => HttpResponse()
+  private val token                                    = AccessToken("token", 60)
+  private val config                                   = system.settings.config.getConfig("dingtalk.ding")
+
+  private var bound: ServerBinding = _
+
+  "Ding" should {
+    "sign timestamp with secret" in {
+      val algo   = "HmacSHA256"
+      val secret = "secret"
+
+      val timestamp = System.currentTimeMillis().toString
+      val mac       = Mac.getInstance(algo)
+      mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), algo))
+
+      val bytes = mac.doFinal(timestamp.getBytes("UTF-8"))
+      Ding.sign(timestamp, secret) shouldBe new String(Base64.getEncoder.encode(bytes))
+    }
+
+    "create location header with uri state" in {
+      val uri =
+        "https://oapi.dingtalk.com/connect/qrconnect?appid=your_app_id&response_type=code&scope=snsapi_login&redirect_uri=%2Fauthorized&state=aHR0cDovL3Rlc3Q="
+      Ding(default).authorization("http://test") shouldBe Location(uri)
+    }
+
+    "authenticated" in {
+      val f = Ding({
+        case (UserInfo("i", "name", Seq(0), "avatar.ico", false, Seq()), _) => HttpResponse()
+      }).authenticate(token, HttpRequest(uri = "/authorized?code=c&state=aHR0cDovL3Rlc3Q="))
+      Await.result(f, 1.second) shouldBe HttpResponse()
+    }
+
+    "complain missing code or state" in {
+      val f = Ding(default).authenticate(token, HttpRequest())
+      Await.result(f, 1.second).status shouldBe StatusCodes.BadRequest
+    }
+
+    "complain invalid token by 40014" in {
+      val f = Ding(default).authenticate(token, HttpRequest(uri = "/authorized?code=40014&state=aHR0cDovL3Rlc3Q="))
+      intercept[IllegalArgumentException](Await.result(f, 1.second)) shouldBe InvalidToken
+    }
+
+    "complain invalid token by 42001" in {
+      val f = Ding(default).authenticate(token, HttpRequest(uri = "/authorized?code=42001&state=aHR0cDovL3Rlc3Q="))
+      intercept[IllegalArgumentException](Await.result(f, 1.second)) shouldBe InvalidToken
+    }
+
+    "complain invalid response by non-json" in {
+      val f = Ding(default).authenticate(token, HttpRequest(uri = "/authorized?code=text&state=aHR0cDovL3Rlc3Q="))
+      intercept[IllegalStateException](Await.result(f, 1.second)).getMessage shouldBe "200 OK - text"
+    }
+
+  }
+
+  override protected def beforeAll(): Unit = {
+    val f = Http().bindAndHandle(mockDingServer, "localhost", 10086)
+    bound = Await.result(f, 1.second)
+  }
+
+  override protected def afterAll(): Unit = {
+    Await.result(bound.unbind().flatMap(_ => system.terminate()), 3.second)
+  }
+
+  private def json(content: String) = HttpEntity(ContentTypes.`application/json`,content)
+
+  private def mockDingServer = concat(
+    (post & path("sns" / "getuserinfo_bycode") & parameters("signature", "timestamp", "accessKey") & entity(as[TmpAuthCode])) {
+      case (signature, timestamp, _, _) if Ding.sign(timestamp, config.getString("secret")) != signature =>
+        complete(Err(-1))
+      case (_, _, accessKey, _) if accessKey != config.getString("appid") =>
+        complete(Err(-1))
+      case (_, _, _, TmpAuthCode("42001")) =>
+        complete(json("""{ "errcode": 0, "errmsg": "ok", "user_info": { "nick": "", "openid": "", "unionid": "42001" } }"""))
+      case (_, _, _, TmpAuthCode("40014")) =>
+        complete(json("""{ "errcode": 0, "errmsg": "ok", "user_info": { "nick": "", "openid": "", "unionid": "40014" } }"""))
+      case (_, _, _, TmpAuthCode("text")) =>
+        complete("text")
+      case (_, _, _, _) =>
+        complete(json("""{ "errcode": 0, "errmsg": "ok", "user_info": { "nick": "", "openid": "", "unionid": "u" } }"""))
+
+    },
+    (get & path("user" / "getUseridByUnionid") & parameters("access_token", "unionid")) {
+      case ("token", "u") =>
+        complete(json(""" { "errcode": 0, "errmsg": "ok", "contactType": 0, "userid": "i" } """))
+      case (_, "40014") =>
+        complete(Err(40014))
+      case (_, "42001") =>
+        complete(json(""" { "errcode": 0, "errmsg": "ok", "contactType": 0, "userid": "42001" } """))
+      case (_, _) =>
+        complete(Err(-1))
+    },
+    (get & path("user" / "get") & parameters("access_token", "userid")) {
+      case ("token", "i") =>
+        complete(UserInfo("i", "name", Seq(0), "avatar.ico", false, Seq.empty))
+      case ("token", "42001") =>
+        complete(Err(42001))
+      case (_, _) =>
+        complete(Err(-1))
+    },
+    (get & path("gettoken") & parameters("appkey", "appsecret")) {
+      case (key, secret) if key == config.getString("appkey") && secret == config.getString("secret") =>
+        complete(token)
+      case (_, _) =>
+        complete(Err(-1))
+    }
+  )
+
+}
+
+object DingSpec {
+  final case class TmpAuthCode(`tmp_auth_code`: String)
+}
