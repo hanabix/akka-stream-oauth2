f3ce4b06efa6f3885ef38cf82739a06fe21fd1b9
zhongl <zhong.lunfu@gmail.com>
2019-01-10 10:18:11 +0800

    Add host matching first test case.

--- core/src/test/scala/zhongl/stream/oauth2/AuthorizedSpec.scala
+++ core/src/test/scala/zhongl/stream/oauth2/AuthorizedSpec.scala
@@ -1,15 +1,20 @@
 package zhongl.stream.oauth2
+import akka.http.scaladsl.model.headers.Host
 import akka.http.scaladsl.model.{HttpRequest, Uri}
 import org.scalatest.{Matchers, WordSpec}
 
 class AuthorizedSpec extends WordSpec with Matchers {
   "Authorized redirect" should {
-    "only path" in {
+    "only matches path" in {
       Authorized(Uri("/path")).unapply(HttpRequest(uri = Uri("http://any/path"))) shouldBe true
     }
 
-    "authority and path" in {
+    "matches both authority and path" in {
       Authorized(Uri("http://spec/path")).unapply(HttpRequest(uri = Uri("http://spec/path"))) shouldBe true
+    }
+
+    "matches Host header first" in {
+      Authorized(Uri("http://spec/path")).unapply(HttpRequest(uri = Uri("http://any/path"), headers = List(Host("spec")))) shouldBe true
     }
   }
 }
