1b1ed86b2cfcb7136b15ad5412872a73e51eb2ee
Lunfu Zhong <zhong.lunfu@gmail.com>
2021-09-05 00:21:29 +0000

    Reformat with scalafmt 3.0.2

--- core/src/main/scala/zhongl/stream/oauth2/OAuth2.scala
+++ core/src/main/scala/zhongl/stream/oauth2/OAuth2.scala
@@ -26,14 +26,18 @@ trait OAuth2[Token] {
   /** @return a new valid access_token for API calling */
   def refresh: Future[Token]
 
-  /** @param token for API calling
-    * @param authorized request redirected with code and state
-    * @return a response with visitor's authenticated information
+  /** @param token
+    *   for API calling
+    * @param authorized
+    *   request redirected with code and state
+    * @return
+    *   a response with visitor's authenticated information
     */
   def authenticate(token: Token, authorized: HttpRequest): Future[HttpResponse]
 
   /** @param state
-    * @return URI will be redirected for authorization
+    * @return
+    *   URI will be redirected for authorization
     */
   def authorization(state: String): Location
 
